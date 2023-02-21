2bf66945496e28363606b843407c22eef7cb4998
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-02-21 08:01:52 +0530

    conditional statement

---
+++ Conditional stmt/Largest3.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter Your First Number:"));
+num2 = float(input("Enter Your Second Number:"));
+num3 = float(input("Enter Your Third Number:"));
+
+if num1>num2 and num1>num3:
+    print("Num 1 is Greater")
+
+elif num2>num1 and num2>num3:
+    print("Num 2 is Greater")
+
+elif num3>num1 and num3>num1:
+    print("Num 3 is Greater")
---
+++ Conditional stmt/test.py
@@ -0,0 +1,11 @@
+a=10;
+b=19;
+
+if a==b:
+ print("a is equal to b");
+if a!=b:
+ print("b is not equal to a");
+elif a>b:
+ print("a is greater");
+elif a>b:
+ print("b is greater");
