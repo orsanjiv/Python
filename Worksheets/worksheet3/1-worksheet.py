ad9f8dcecd310513b992163763b9707224352398
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-02-28 13:42:54 +0530

    Update 1-worksheet.py

--- Worksheets/worksheet3/1-worksheet.py
+++ Worksheets/worksheet3/1-worksheet.py
@@ -1,7 +1,9 @@
 #Program to demonstrate the use of functions and passing different types of arguments in functions.
 def add(num1,num2):
-    print("sum of two number is:",num1+num2);
+    sum=num1+num2
+    return sum
 
 a=eval(input("num1:"))
 b=eval(input("num2:"))
-add(a,b);
+sum = add(a,b);
+print(sum);
