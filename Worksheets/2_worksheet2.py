5c8dc3b0a87076818fac7e63f4d5646259319d92
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-02-28 11:23:40 +0530

    Update 2_worksheet2.py

--- Worksheets/2_worksheet2.py
+++ Worksheets/2_worksheet2.py
@@ -1,10 +1,10 @@
 num = eval(input("Enter a number: "))
 sum = 0
-
+power = len(str(num));
 temp = num
 while temp != 0:
    digit = temp % 10
-   sum += digit ** 3
+   sum += digit ** power
    temp //= 10
 
 if num == sum:
