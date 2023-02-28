203f81d90bd0736c803e88c04142c5c111a0a871
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-02-28 14:39:30 +0530

    Update 1-worksheet.py

--- Worksheets/worksheet3/1-worksheet.py
+++ Worksheets/worksheet3/1-worksheet.py
@@ -1,9 +1,32 @@
 #Program to demonstrate the use of functions and passing different types of arguments in functions.
-def add(num1,num2):
-    sum=num1+num2
-    return sum
+# docstring must me there
+#write a python program to calculate the area of 10 differet circle,given the PI=22/7 AND RADIUS OF CRICLES ENTERNED BY USER USING SIMPLE FUNCTION,PARAMETERIZED FUNCTION, RETURNED TYPE WITH FUNCTION AND RETURN TYPE WITH PARAMETERIZED FUNCTION
+#Write a python program to print multiplication table from 2 to 20 ,where table values are entered by the user by using simple function,parameterized function,return with function and return type with parameterized function.
 
-a=eval(input("num1:"))
-b=eval(input("num2:"))
-sum = add(a,b);
-print(sum);
+
+PI=3.14
+
+def simple_area(radius):
+    area = PI*radius*radius;
+    print(area);
+
+def area_para(PI,radius):
+    area = PI*radius*radius;
+    print(area)
+
+def area_ret(radius):
+    return PI*radius*radius
+
+def area_ret_para(PI,radius):
+    return PI*radius*radius
+
+
+radius =[1,2,3,4,5,6,7,8,9,10];
+print("Simple function")
+for i in radius:
+    simple_area(i);
+
+
+
+
+
