8e794f1a0b7b43a3278f4a654d9195a4e900cfde
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-03-03 00:18:26 +0530

    Update 2-worksheet.py

--- Worksheets/worksheet3/2-worksheet.py
+++ Worksheets/worksheet3/2-worksheet.py
@@ -2,8 +2,7 @@ def simp_multi(num):
     for i in range(1,11):
         print(num,"*",i,"=",i*num);
 
-def take_input():
-    for i in range(2,21):
-        num=eval(input("Enter a number to print table:"))
-        print("Table of ",num,"is:")
-        simp_multi(num);
+
+num=eval(input("Enter a number to print table:"))
+print("Table of ",num,"is:")
+simp_multi(num);
