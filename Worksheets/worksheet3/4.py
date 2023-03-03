42b7f241f784108394aaac7462bffd7ada0cc7d1
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-03-03 10:37:55 +0530

    Update 4.py

--- Worksheets/worksheet3/4.py
+++ Worksheets/worksheet3/4.py
@@ -2,13 +2,41 @@ def simp_multi(num):
     for i in range(1,11):
         print(num,"*",i,"=",i*num)
 
+def argu_multi(num,i):
+    for i in range(10):
+        print(num,"*",i,"=",i*num)
+
+
 def generate_table(num):
     table = []
     for i in range(1, 11):
       table.append(num*i)
     return table
 
+def print_table(num):
+    table = generate_table(num)
+    for i in range(10):
+     print(num, "x", i+1, "=", table[i])
+
+def generate_table_ret(num):
+    table = []
+    for i in range(1, 11):
+      table.append(num*i)
+    return table
+
+def print_table(num):
+    table = generate_table_ret(num,i)
+    for i in range(10):
+     print(num, "x", i+1, "=", table[i])     
+
+
 num=eval(input("Number likh:"));
-simp_multi(num);
-pri=generate_table(num)
-print(pri);
+i=1;
+print("\nSimple Function:")
+simp_multi(num)
+
+print("\nFunction With Arguments:")
+argu_multi(num,i);
+
+print("\nReturn Type:")
+print_table(num)     
