bac7510370b36d79e0396d8803dea4e06c5b1466
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-03-17 13:38:16 +0530

    worksheet4

---
+++ Worksheets/worksheet4/new.py
@@ -0,0 +1,26 @@
+s1 = "namaste duniya"
+# for i in s1:
+#     print(i)
+
+# for i in s1:
+#     print(i,end="-")
+
+# printing alternater string 
+# for i in range(0,len(s1),2):
+#     print(s1[i],end=" ")
+
+# operation on string
+# concatenation
+# s2 = "and hello world"
+# print(s1+ " " + s2);
+
+# repetion
+# print(s1*3)
+
+
+# "in" and "not in " operator
+# print("namaste" in s1)
+# print("namaste" not in s1)
+
+# string comparison
+print("free"=="Free")
