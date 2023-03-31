19b4e5f163c1733a23e7475e8fb3e72878922d6d
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-03-31 11:03:21 +0530

    exp5

--- Loop/NestedForLoop.py
+++ Loop/NestedForLoop.py
@@ -1,4 +1,4 @@
 for i in range(1,4,1):
   for j in range(1,4,1):
-   print("i", i, "j", j)
+   print("i=", i, "j=", j)
 print("End of the loop")
---
+++ Worksheets/worksheet5/1.py
@@ -0,0 +1,36 @@
+# l = [(2,5),(1,2),(4,4),(2,3),(2,1)]
+
+# for i in range(len(l)):
+#     for j in range(len(l)):
+#         if l[i][1]<l[j][1]:
+#             l[i],l[j] = l[j],l[i]
+
+# print(l)
+
+# def last(n):
+#     return n[-1]  
+ 
+# def sort(tuples):
+#     return sorted(tuples, key=last)
+ 
+# a=input("Enter a list of tuples:")
+# print("Sorted:")
+# print(sort(a))
+
+def get_last_element(tup):
+    return tup[-1]
+def sort_tuples(tuple_list):
+    return sorted(tuple_list,key=get_last_element)
+
+n = int(input("Enter the Number of Tuple:"))
+tuple_list = []
+
+for i in range(n):
+    tup_str = input(f"Enter tuple {i+1}: ")
+    tup_list = tup_str.split()
+    tup = tuple(int(elem) for elem in tup_list)
+    tuple_list.append(tup)
+
+print("Initial list: ",tuple_list)
+sorted_list = sort_tuples(tuple_list)
+print("Sorted list:",sorted_list)
