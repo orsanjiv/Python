d8534a3217476689d664efdeed974607697bc911
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-05-05 11:27:31 +0530

    update

--- Worksheets/worksheet 8/bin.py
+++ Worksheets/worksheet 8/bin.py
@@ -1,11 +1,11 @@
-def binary_search(list1, n):
+def binary_search(list1, Key):
   low = 0
   high = len(list1) - 1 
   while low <= high:
    mid = (high + low) // 2 
-   if list1[mid] < n:
+   if list1[mid] < Key:
     low = mid + 1 
-   elif list1[mid] > n: 
+   elif list1[mid] > Key: 
     high = mid - 1
    else:
     return mid 
@@ -18,7 +18,7 @@ for i in range(0, n):
     list1.append(element) 
 Key = int(input("Enter Key to search : ")) 
 
-result = binary_search(list1, n) 
+result = binary_search(list1, Key) 
 if(result == -1):
  print("Element not found")
 else:
