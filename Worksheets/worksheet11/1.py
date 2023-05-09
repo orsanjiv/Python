10ba2366b9614d01c8bcb0fa1b4dc99b29c17559
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-05-09 13:47:13 +0530

    update 11

--- Worksheets/worksheet11/1.py
+++ Worksheets/worksheet11/1.py
@@ -1,9 +1,20 @@
+# write
 obj1 =  open("demo.txt","w")
 obj1.write("namaste duniya")
 obj1.close()
 
-obj1 =  open("demo.txt","r")
-z=obj1.read()
+# read
+# obj1 =  open("demo.txt","r")
+# z=obj1.read()
+# print(z)
+
+# append
+obj1 = open("demo.txt","a")
+obj1.write("\nnew namaste duniya")
+obj1.close()
+
+obj1 = open("demo.txt","r")
+z = obj1.read()
 print(z)
 
 
