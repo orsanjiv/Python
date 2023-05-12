3ffe01a90c84a19410fa0b56d2e951e2a5531f99
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-05-12 11:01:41 +0530

    Update 11.py

--- Worksheets/worksheet11/11.py
+++ Worksheets/worksheet11/11.py
@@ -4,3 +4,8 @@ os.chdir('C:/Users/SANJIV/Documents/GitHub/Python/Worksheets/worksheet11/textFil
 for letter in string.ascii_uppercase:   
   with open(letter + ".txt", "w") as f:       
     f.writelines(letter)
+
+# "with" statement ensure that the file should be closed after execution
+# block of code
+# "writelines" used to write a list of strings to a file
+# "as f" part of the with statement is used to create a file object and assign it to the variable f
